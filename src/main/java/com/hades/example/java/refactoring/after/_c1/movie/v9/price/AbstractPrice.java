3b5552ec28e131ee427d836d10d292ea389ed4eb
YingVickyCao <ying.vicky.cao@outlook.com>
2020-07-22 00:27:47 +0800

    refactor

--- src/main/java/com/hades/example/java/refactoring/after/_c1/movie/v9/price/AbstractPrice.java
+++ src/main/java/com/hades/example/java/refactoring/after/_c1/movie/v9/price/AbstractPrice.java
@@ -1,4 +1,4 @@
-package com.hades.example.java.refactoring.after._c1.movie.v8.price;
+package com.hades.example.java.refactoring.after._c1.movie.v9.price;
 
 public abstract class AbstractPrice {
     public abstract int getPrice();
---
+++ src/main/java/com/hades/example/java/refactoring/after/_c1/movie/v9/price/NewReleasePrice.java
@@ -0,0 +1,8 @@
+package com.hades.example.java.refactoring.after._c1.movie.v9.price;
+
+public class NewReleasePrice extends AbstractPrice {
+    @Override
+    public int getPrice() {
+        return 0;
+    }
+}
