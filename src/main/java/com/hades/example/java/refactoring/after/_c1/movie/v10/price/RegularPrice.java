ebc28f188631b14c11f6ad122d4df17149402d2f
YingVickyCao <ying.vicky.cao@outlook.com>
2020-07-22 07:42:50 +0800

    refactor

---
+++ src/main/java/com/hades/example/java/refactoring/after/_c1/movie/v10/price/RegularPrice.java
@@ -0,0 +1,18 @@
+package com.hades.example.java.refactoring.after._c1.movie.v10.price;
+
+import com.hades.example.java.refactoring.after._c1.movie.v10.Movie;
+
+public class RegularPrice extends Price {
+    @Override
+    public int getPriceCode() {
+        return Movie.REGULAR;
+    }
+
+    public double getCharge(final int daysRented) { // each -> rental
+        double result = 0;
+        result += 2;
+        if (daysRented > 2)
+            result += (daysRented - 2) * 1.5;
+        return result;
+    }
+}
