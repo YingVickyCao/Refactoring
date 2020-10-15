0f5b033eea084cf6cc7920ccc2dc3a38d3afeb13
YingVickyCao <ying.vicky.cao@outlook.com>
2020-10-15 21:30:33 +0800

    。

---
+++ src/main/java/com/hades/example/java/refactoring/after/_c7/_c7_6_remov_moiddle_man/Example.java
@@ -0,0 +1,67 @@
+package com.hades.example.java.refactoring.after._c7._c7_6_remov_moiddle_man;
+
+public class Example {
+    public static void main(String[] args) {
+        new Example().test();
+    }
+
+    class Person {
+        private Department _department;
+
+        public Person() {
+        }
+        // SESSION:移除委托函数,START
+//        public Manager getManager() {
+//            return _department.getManager();
+//        }
+//        public String getDepartmentName() {
+//            return _department.getDepartmentName();
+//        }
+
+        public Department getDepartment() {
+            return _department;
+        }
+        // SESSION:移除委托函数,END
+
+        public void setDepartment(Department department) {
+            _department = department;
+        }
+    }
+
+    class Manager extends Person {
+    }
+
+    class Department {
+        private Manager manager;
+        private String departmentName = "Mock Department name";
+
+        public Department(Manager manager) {
+            this.manager = manager;
+        }
+
+        public Manager getManager() {
+            return manager;
+        }
+
+        public String getDepartmentName() {
+            return departmentName;
+        }
+    }
+
+    private void test() {
+        Manager manager = new Manager();
+        Department department = new Department(manager);
+
+        Person person = new Person();
+        person.setDepartment(department);
+
+        // 找出某人的经理
+        // SESSION:移除委托函数,START
+//        person.getManager();
+//        person.getDepartmentName();
+        Department department2 = person.getDepartment();
+        department2.getManager();
+        department2.getDepartmentName();
+        // SESSION:移除委托函数,END
+    }
+}
---
+++ src/main/java/com/hades/example/java/refactoring/after/_c7/_c7_7_introduce_forign_method/Example.java
@@ -0,0 +1,45 @@
+package com.hades.example.java.refactoring.after._c7._c7_7_introduce_forign_method;
+
+public class Example {
+    public static void main(String[] args) {
+        new Example().test();
+    }
+
+    private void test() {
+        Date previousEnd = new Date(2020, 11, 13);
+
+        // 客户类：Example
+        Date newStart = nextDay(previousEnd);
+        Date newStart2 = nextDay(newStart);
+        Date newStart3 = nextDay(newStart2);
+    }
+
+    private Date nextDay(final Date previousEnd){
+        // TODO: foreign method, should on date
+        return new Date(previousEnd.getYear(), previousEnd.getMonth(), previousEnd.getDate() + 1);
+    }
+    // 服务类：Date
+    class Date {
+        int year;
+        int month;
+        int date;
+
+        public Date(int year, int month, int date) {
+            this.year = year;
+            this.month = month;
+            this.date = date;
+        }
+
+        int getYear() {
+            return year;
+        }
+
+        int getMonth() {
+            return month;
+        }
+
+        int getDate() {
+            return date;
+        }
+    }
+}
