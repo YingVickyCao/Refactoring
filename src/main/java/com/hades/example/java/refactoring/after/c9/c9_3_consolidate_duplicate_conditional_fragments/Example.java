582a98a198935e3e0623288ed5bdc03aaeb8afff
YingVickyCao <ying.vicky.cao@outlook.com>
2021-01-07 21:47:54 +0800

    9.4 Remove Control Flag（245）（移除控制标记）

--- src/main/java/com/hades/example/java/refactoring/after/c9/c9_3_consolidate_duplicate_conditional_fragments/Example.java
+++ src/main/java/com/hades/example/java/refactoring/after/c9/c9_3_consolidate_duplicate_conditional_fragments/Example.java
@@ -1,53 +1,29 @@
 package com.hades.example.java.refactoring.after.c9.c9_3_consolidate_duplicate_conditional_fragments;
 
 public class Example {
-    // Example 1: 以break 取代简单的控制标记
-    // 检查是否包含可疑人员
-    void checkSecurity(String[] people) {
-        for (String person : people) {
-            if (person.equals("Tom")) {
-                sendAlert();
-                break;
-            }
+    private double _total;
+    private double _price = 15;
 
-            if (person.equals("Jerry")) {
-                sendAlert();
-                break;
-            }
+    double getTotal() {
+        if (isSpecialDeal()) {
+            _total = _price * 0.95;
+        } else {
+            _total = _price * 0.98;
         }
+        send();
+        return _total;
     }
 
-    private void sendAlert() {
+    boolean isSpecialDeal() {
+        return true;
     }
 
-    // Example 2: 以return 返回控制标记
-    // 检查是否包含可疑人员
-    void checkSecurity2(String[] people) {
-        String found = foundMiscreant(people);
-        someLaterCode(found);
-    }
-
-    String foundMiscreant(String[] people) {
-        for (String person : people) {
-            if (person.equals("Tom")) {
-                sendAlert();
-                return person;
-            }
-
-            if (person.equals("Jerry")) {
-                sendAlert();
-                return person;
-            }
-        }
-        return "";
-    }
-
-    void someLaterCode(String found) {
-        System.out.println(found);
+    void send() {
+        System.out.println(_total);
     }
 
     public static void main(String[] args) {
         Example example = new Example();
-        example.checkSecurity(new String[]{"People 1", "Tom", "People 2"});
+        example.getTotal();
     }
 }
--- src/main/java/com/hades/example/java/refactoring/before/c9/c9_3_consolidate_duplicate_conditional_fragments/Example.java
+++ src/main/java/com/hades/example/java/refactoring/before/c9/c9_3_consolidate_duplicate_conditional_fragments/Example.java
@@ -1,55 +1,30 @@
 package com.hades.example.java.refactoring.before.c9.c9_3_consolidate_duplicate_conditional_fragments;
 
 public class Example {
-    // Example 1: 以break 取代简单的控制标记
-    // 检查是否包含可疑人员
-    void checkSecurity(String[] people) {
-        // found 做了1件事情：控制标记
-        boolean found = false;
-        for (String person : people) {
-            if (!found) {
-                if (person.equals("Tom")) {
-                    sendAlert();
-                    found = true;
-                }
+    private double _total;
+    private double _price = 15;
 
-                if (person.equals("Jerry")) {
-                    sendAlert();
-                    found = true;
-                }
-            }
+    double getTotal() {
+        if (isSpecialDeal()) {
+            _total = _price * 0.95;
+            send();
+        } else {
+            _total = _price * 0.98;
+            send();
         }
+        return _total;
     }
 
-    private void sendAlert() {
+    boolean isSpecialDeal() {
+        return true;
     }
 
-    // Example 2: 以return 返回控制标记
-    // 检查是否包含可疑人员
-    void checkSecurity2(String[] people) {
-        // found 做了2件事情：控制标记，运算结果
-        String found = "";
-        for (String person : people) {
-            if (found.equals("")) {
-                if (person.equals("Tom")) {
-                    sendAlert();
-                    found = person;
-                }
-
-                if (person.equals("Jerry")) {
-                    sendAlert();
-                    found = person;
-                }
-            }
-        }
-    }
-
-    void someLaterCode(String found) {
-        System.out.println(found);
+    void send() {
+        System.out.println(_total);
     }
 
     public static void main(String[] args) {
         Example example = new Example();
-        example.checkSecurity(new String[]{"People 1", "Tom", "People 2"});
+        example.getTotal();
     }
 }
